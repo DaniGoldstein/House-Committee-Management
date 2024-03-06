5f4949c32c473c705ae6d017e902ab347f2b1b11
danielgoldshtein <danigold054@gmail.com>
2024-03-06 17:53:55 +0200

    usenavigate

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
+import {useNavigate } from "react-router-dom"
 import style from './style.module.css';
 import axios from 'axios';
 
+
+
 export default function Login() {
 
+  const navigate = useNavigate();
   const sendValue = {
     userName: "",
     password: "",
@@ -15,46 +19,62 @@ export default function Login() {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
-    console.log(formValues);
+   
 
   }
   const handleSubmit = (e) => {
+
     e.preventDefault();
+
     setFormErrors(validate(formValues));
 
-    checkSucceededSubmit();
+
+
 
   }
 
   const validate = (value) => {
     const errors = {};
-    if (!value.username) {
-      errors.userName = "דרוש שם משתמש"
+
+    if (value.userName.length > 0 && value.password.length >= 4) {
+      send();
     }
-    if (!value.password) {
-      errors.password = " דרושה סיסמא "
+    else {
+      if (value.userName == 0) {
+        errors.userName = "דרוש שם משתמש"
+      }
+      if (value.password.length == 0) {
+        errors.password = " דרושה סיסמא "
+      }
+      else if (value.password.length < 4) {
+        errors.password = "סיסמא קצרה מידי"
+      }
     }
-    else if (value.password.length < 4) {
-      errors.password = "סיסמא קצרה מידי"
-    }
+
     return errors
   }
 
-  const checkSucceededSubmit = async () => {
-    if (!formErrors.password && !formErrors.userName) {
-      try{
-        console.log("aaaaaa");
-      let token =await axios.post('http://localhost:3535/login', {
+  const send = async () => {
+    console.log(formValues);
+
+    try {
+
+      let response = await axios.post('http://localhost:3535/login',{},{
         headers: {
           username: formValues.userName,
           password: formValues.password
         }
-      }).data;
-      console.log(token);
+      })
+      console.log(response.data);
+      if (response.data) {
+        console.log("acces");
+        navigate("/homePortal");
+      }
     }
-  catch(err){
-    console.log(err);
-  }}
+    catch (err) {
+      console.log(err);
+
+    }
   }
 
   return (
