77cb8e5d100137d0cf0ee6f2ae02a692032e2f59
danielgoldshtein <danigold054@gmail.com>
2024-06-10 13:45:01 +0300

    showMainMessage

--- src/contentPages/mainMessages/MainMessages.jsx
+++ src/contentPages/mainMessages/MainMessages.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react'
 
 import allBuildingContext from '../../BuildingContext';
 import Table3columns from '../../components/table/Table3columns';
+import Message from '../../components/message/Message';
 
 
 
@@ -10,13 +11,15 @@ export default function MainMessages() {
 
   const { allBuildingDetails } = useContext(allBuildingContext);
 
-  const [messages, setMessages] = useState('');
+  const [messages, setMessages] = useState();
 
-  useEffect(() => {
-    console.log("messages");
+
+ 
+  useEffect(() => {console.log("useEffect in mainMessages");
+    allBuildingDetails && console.log(allBuildingDetails.generalMessages,"from main messages");
     allBuildingDetails && setMessages(allBuildingDetails.generalMessages);
    
-  },[])
+  },[allBuildingDetails])
 
 
 
@@ -28,9 +31,12 @@ export default function MainMessages() {
 
     <div>
             <h1 class=" text-2xl font-semibold"> הודעות וועד הבית</h1>
-      {messages.length == 0 ? "אין הודעות" :
-        <Table3columns th2={"הודעה"} th3={"תאריך"} arrContent={messages} />
-      }
+      {messages && messages.length == 0 ? "אין הודעות" :
+      
+      
+      messages && messages.map((message)=>   <div>
+        <Message name="ועד הבית" date={message.date.slice(0,10)} content={message.title}/> <br></br>
+        </div> )}
 
     </div>
 
